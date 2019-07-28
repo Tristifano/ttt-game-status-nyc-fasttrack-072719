f3713a7c2e3a88353a6ae0c4590f73adf02df6a7
Tristan Myers <tristifano@gmail.com>
2019-07-28 18:51:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,5 +7,8 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 
 def won?(board)
-  board.each do |position|
-   position_taken
+  for win_combo in WIN_COMBINATIONS
+    win_combo.each do |position|
+        position_taken?(board,position)
+        
+        
